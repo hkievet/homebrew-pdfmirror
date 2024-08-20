9bf984ec1b9c3c16e75dee56bfd5e1477493434a
Hunter Kievet <hkievet@gmail.com>
2024-08-20 11:58:05 -0700

    this is so complicated...

--- Formula/pdfmirror.rb
+++ Formula/pdfmirror.rb
@@ -4,7 +4,7 @@ class Pdfmirror < Formula
     desc "A CLI tool for mirroring PDF"
     homepage "https://github.com/hkievet/homebrew-pdfmirror"
     url "https://github.com/hkievet/homebrew-pdfmirror/releases/download/v0.1.8/pdfmirror-0.1.0.tar.gz"  # Link to the tarball of your package
-    sha256 "cdca62fe6902fc57c5928d22e778993ac852351804d8d684a4515da710bc80ab"  # Replace with the actual SHA-256 hash
+    sha256 "7567eb91d57fdbcb6267ef44a1a459629e2aeb5b9b188229c7b784f20c16f4b2"  # Replace with the actual SHA-256 hash
     license "MIT"
   
     depends_on "python@3.9"  # or whichever Python version you need
