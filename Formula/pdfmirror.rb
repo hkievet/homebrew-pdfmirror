cbd3aef422993604f41f0ff3b12c1d96a07d962f
Hunter Kievet <hunterkievet@macbookpro.home>
2025-11-30 16:21:22 -0800

    Fix: Use virtualenv_install_with_resources and install pymupdf from wheel

--- Formula/pdfmirror.rb
+++ Formula/pdfmirror.rb
@@ -5,20 +5,16 @@ class Pdfmirror < Formula
     homepage "https://github.com/hkievet/homebrew-pdfmirror"
     url "https://github.com/hkievet/homebrew-pdfmirror/releases/download/v0.1.8/pdfmirror-0.1.0.tar.gz"  # Link to the tarball of your package
     sha256 "7567eb91d57fdbcb6267ef44a1a459629e2aeb5b9b188229c7b784f20c16f4b2"  # Replace with the actual SHA-256 hash
-    version "0.1.3"  # Explicit version to force upgrade recognition
+    version "0.1.4"  # Explicit version to force upgrade recognition
     license "MIT"
   
     depends_on "python@3.9"  # or whichever Python version you need
     depends_on "tesseract"   # Adding tesseract as a dependency
   
     def install
-      venv = virtualenv_create(libexec)
-      # Install build tools and pymupdf
-      venv.pip_install "setuptools"
-      venv.pip_install "wheel"
-      venv.pip_install "pymupdf"
-      # Install the package and link binaries (this should also install dependencies from setup.py)
-      venv.pip_install_and_link buildpath
+      virtualenv_install_with_resources
+      # Explicitly install pymupdf using only-binary to use pre-built wheels
+      system "#{libexec}/bin/pip", "install", "--only-binary", ":all:", "pymupdf"
     end
   
     test do
